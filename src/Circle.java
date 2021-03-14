68dcdf5c5f48c1535da44c89bc41f8f4bff3c74d
spralja <spralja@ruc.dk>
2021-03-14 01:13:07 +0100

    Create Circle.java

---
+++ src/Circle.java
@@ -0,0 +1,44 @@
+public class Circle extends Shape{
+    private Point c = new Point();
+    private double r = 0.0;
+
+    public Circle() {}
+    public Circle(Point c, double r) {
+        this.c = c;
+        this.r = r;
+    }
+
+    public Circle(double x, double y, double r) {
+        this.c = new Point(x, y);
+        this.r = r;
+    }
+
+    public Circle(Circle that) {
+        this.c = that.c;
+        this.r = r;
+    }
+    
+    @Override
+    public Point getCenter() {
+        return null;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getCircumference() {
+        return 0;
+    }
+
+    @Override
+    public boolean isInside(Point point) {
+        return false;
+    }
+
+    public boolean equals(Circle that) {
+        return false;
+    }
+}
